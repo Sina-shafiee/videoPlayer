e451185185dedaa313a409e14126fa420403cc23
Sina <alisinashafieezada@gmail.com>
2023-04-12 11:16:09 +0430

    api protocol fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://exkenas.ir/api';
+export const API_URL = 'https://exkenas.ir/api';
 export const mimeType = 'video/webm; codecs="opus,vp8"';
 export const MAX_VIDEO_LENGTH = 20000; // 20sec
