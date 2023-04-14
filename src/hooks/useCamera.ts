1243fed69481c4a24abb5207b1b40e552e0285aa
Sina <alisinashafieezada@gmail.com>
2023-04-14 15:09:16 +0430

    safari fix

--- src/hooks/useCamera.ts
+++ src/hooks/useCamera.ts
@@ -4,7 +4,7 @@ import { toast } from 'react-hot-toast';
 type TReturn = [boolean, () => Promise<void>, MediaStream | null];
 
 const videoConstraints = {
-  audio: false,
+  // audio: false,
   video: {
     width: 1280,
     height: 720
