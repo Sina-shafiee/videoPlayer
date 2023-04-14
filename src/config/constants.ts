56cd42c8017e062f34a35fe6d55017ceb312d891
Sina <alisinashafieezada@gmail.com>
2023-04-14 14:19:04 +0430

    safari test

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,4 @@
 export const API_URL = 'https://exkenas.ir/api';
-export const mimeType = 'video/webm; codecs="opus,vp8"';
-export const CHROME_MEME_TYPE = 'video/webm; codecs="opus,vp8"';
-export const SAFARI_MEME_TYPE = 'video/mp4;codecs:h264';
+export const OTHER_OS_MIME_TYPE = 'video/webm; codecs="opus,vp8"';
+export const MAC_MIME_TYPE = 'video/mp4;codecs:h264';
 export const MAX_VIDEO_LENGTH = 20000; // 20sec
