f61702383c3baa954da354fcdce8802e6d674b02
Sina <alisinashafieezada@gmail.com>
2023-04-14 13:46:53 +0430

    safari test

--- src/components/VideoRecorder.tsx
+++ src/components/VideoRecorder.tsx
@@ -35,7 +35,9 @@ const VideoRecorder = memo(() => {
 
         // value is lower than 100 then update progress bar with that value else if value is equal to 100 set progress value to 0 in order to hide progress bar during another recordings
         if (percentCompleted < 100) {
-          setUploadProgress(percentCompleted);
+          setTimeout(() => {
+            setUploadProgress(percentCompleted);
+          }, 500);
         } else if (percentCompleted === 100) {
           setUploadProgress(0);
         }
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,5 @@
 export const API_URL = 'https://exkenas.ir/api';
 export const mimeType = 'video/webm; codecs="opus,vp8"';
+export const CHROME_MEME_TYPE = 'video/webm; codecs="opus,vp8"';
+export const SAFARI_MEME_TYPE = 'video/mp4;codecs:h264';
 export const MAX_VIDEO_LENGTH = 20000; // 20sec
